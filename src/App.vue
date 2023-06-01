6455fa3a85e90f05d02717ffb7e44abfa259928f
Jonathan <jonathan.araujo76@icloud.com>
2023-06-01 16:05:22 -0300

    Primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main>
+    <BarraLateral />
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+import BarraLateral from './components/BarraLateral.vue';
+
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    BarraLateral
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
